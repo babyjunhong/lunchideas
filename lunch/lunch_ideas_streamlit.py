53eea16caa37070b959f0729f4ecd3aeaf2f599e
babyjunhong <ameliadesigner522@gmail.com>
2025-12-02 14:52:45 +0800

    Fix button text and streamline lunch choice display

--- lunch/lunch_ideas_streamlit.py
+++ lunch/lunch_ideas_streamlit.py
@@ -70,6 +70,6 @@ st.markdown('<div class="title">🍴 LETS LUNCH!</div>', unsafe_allow_html=True)
 st.markdown('<div class="sub-title">🍴 점심 먹자!</div>', unsafe_allow_html=True)
 
 # Center button
-if st.button("Lets lunch!"):
-    lunch_choice = random.choice(LUNCH_IDEAS)
-    st.markdown(f'<div class="lunch-box">{lunch_choice}</div>', unsafe_allow_html=True)
+if st.button("Let's lunch!"): lunch_choice = random.choice(LUNCH_IDEAS) 
+    st.markdown(f'{lunch_choice}', unsafe_allow_html=True)
+
